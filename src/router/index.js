0d3321acb9746d15d349f398e4a993f5b5597929
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-06-07 14:41:51 -0500

    feat(routing): adding route for galley

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../public/pages/LadingPage.vue'
+import Gallery from '../comics/components/gallery.component.vue'
 
 const routes = [
   { path: '/', component: Home },
+    { path: '/comics', component: Gallery }
 ]
 
 const router = createRouter({
