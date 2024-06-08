d06ab738619a79c78f484ffc747106abadab03d4
Sergio Pecan <u20201a938@upc.edu.pe>
2024-06-08 15:55:57 -0500

    feat(Completed): fronted terminated

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,16 @@ import Home from '../public/pages/LadingPage.vue'
 import Gallery from '../comics/components/gallery.component.vue'
 import Login from "../Login/components/Login/Login.vue";
 import CreateAccount from "../Login/components/create-account/CreateAccount.vue";
-
-
+import recomendationContentComponent from "../recommendations/component/recomendation-content.component.vue";
+import ShoppingcartContent from "../shopping-cart/component/shoppingcart-content.component.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes :[
     { path: '/', component: Home },
     { path: '/comics', component: Gallery },
+    { path: '/recomendation/:id', component: recomendationContentComponent },
     { path: '/Login', component: Login },
+    { path: '/ShoppingCart', component: ShoppingcartContent },
     { path: '/create-account', component: CreateAccount },
     { path: '/:pathMatch(.*)*', redirect: '/' },
 
