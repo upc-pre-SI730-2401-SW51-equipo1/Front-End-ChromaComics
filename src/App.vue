bc1905855f7b580c23beb18974b0111f59d818bc
GoodFries04 <114363212+GoodFries04@users.noreply.github.com>
2024-06-07 20:20:26 -0500

    style: added new styles for login pages

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script >
+import useAuth from "./Login/service/useAuth.js";
+import Cookies from "js-cookie";
 export default {
-  name: 'App'
+  name: 'App',
+
+  created() {
+    const {isAuthenticated, setAuthenticated} = useAuth();
+    const isAuth = Cookies.get('isAuthenticated') === 'true';
+    setAuthenticated(isAuth);
+  }
 }
+
 </script>
 
 <template>
-<router-view/>
+  <router-view/>
 
 </template>
 
-<style >
+<style>
 </style>
