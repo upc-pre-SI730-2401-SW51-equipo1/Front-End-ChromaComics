fb0a39dd6219cfbcd9cca3e7b81a9cc8f5487d06
GoodFries04 <114363212+GoodFries04@users.noreply.github.com>
2024-06-07 20:21:14 -0500

    feat: added new routes for login pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../public/pages/LadingPage.vue'
 import Gallery from '../comics/components/gallery.component.vue'
+import Login from "../Login/components/Login/Login.vue";
+import CreateAccount from "../Login/components/create-account/CreateAccount.vue";
 
-const routes = [
-  { path: '/', component: Home },
-    { path: '/comics', component: Gallery }
-]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes :[
+    { path: '/', component: Home },
+    { path: '/comics', component: Gallery },
+    { path: '/Login', component: Login },
+    { path: '/create-account', component: CreateAccount },
+    { path: '/:pathMatch(.*)*', redirect: '/' },
+
+  ]
 })
 
 export default router
