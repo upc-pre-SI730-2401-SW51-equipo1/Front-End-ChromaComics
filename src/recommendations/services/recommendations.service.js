1e8d86a9d7cbefb65651a1fb12a9052bea10f347
Huaman-Miguel <112042418+Huaman-Miguel@users.noreply.github.com>
2024-06-26 19:50:12 -0500

    Update recommendations.service.js

--- src/recommendations/services/recommendations.service.js
+++ src/recommendations/services/recommendations.service.js
@@ -3,28 +3,28 @@ import { Comic } from "../model/Comic.entity.js";
 
 class RecommendationsService {
     getAll() {
-        return http.get('/comics');
+        return http.get('/recommendation');
     }
 
     getById(id) {
-        return http.get(`/comics/${id}`);
+        return http.get(`/recommendation/${id}`);
     }
 
     create(data) {
-        return http.post('/comics', data);
+        return http.post('/recommendation', data);
     }
 
     update(id, data) {
-        return http.put(`/comics/${id}`, data);
+        return http.put(`/recommendation/${id}`, data);
     }
 
     delete(id) {
-        return http.delete(`/comics/${id}`);
+        return http.delete(`/recommendation/${id}`);
     }
 
     async getAllComics() {
         try {
-            const response = await http.get('/comics');
+            const response = await http.get('/recommendation');
             if (response.data && Array.isArray(response.data)) {
                 return response.data.map(comic => Comic.fromDisplayableComic(comic));
             } else {
